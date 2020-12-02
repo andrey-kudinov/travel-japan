cef90fdcf39508beb4f2409b92e0d75025713049
AndreyKudinov63 <64890665+AndreyKudinov63@users.noreply.github.com>
2020-12-02 22:58:40 +0400

    добавил иконки, сделал мелкие правки

--- js/main-es6.js
+++ js/main-es6.js
@@ -17,7 +17,7 @@
   const burgerItem = document.querySelector(".burger");
   const menu = document.querySelector(".header__nav");
   const menuCloseItem = document.querySelector(".header__nav-close");
-  const menuLinks = document.querySelectorAll('.header__link')
+  const menuLinks = document.querySelectorAll(".header__link");
   burgerItem.addEventListener("click", () => {
     menu.classList.add("header__nav_active");
   });
@@ -26,49 +26,47 @@
   });
   if (window.innerWidth < 768) {
     for (let i = 0; i < menuLinks.length; i++) {
-      menuLinks[i].addEventListener('click', () => {
-        menu.classList.remove("header__nav_active")
-      })
+      menuLinks[i].addEventListener("click", () => {
+        menu.classList.remove("header__nav_active");
+      });
     }
   }
 })();
 
 // Scroll to anchors
 (function () {
-
   const smoothScroll = function (targetEl, duration) {
-      const headerElHeight = document.querySelector('header').clientHeight;
-      let target = document.querySelector(targetEl);
-      let targetPosition = target.getBoundingClientRect().top - headerElHeight;
-      let startPosition = window.pageYOffset;
-      let startTime = null;
-  
-      const ease = function(t,b,c,d) {
-          t /= d / 2;
-          if (t < 1) return c / 2 * t * t + b;
-          t--;
-          return -c / 2 * (t * (t - 2) - 1) + b;
-      };
-  
-      const animation = function(currentTime){
-          if (startTime === null) startTime = currentTime;
-          const timeElapsed = currentTime - startTime;
-          const run = ease(timeElapsed, startPosition, targetPosition, duration);
-          window.scrollTo(0,run);
-          if (timeElapsed < duration) requestAnimationFrame(animation);
-      };
-      requestAnimationFrame(animation);
+    const headerElHeight = document.querySelector("header").clientHeight;
+    let target = document.querySelector(targetEl);
+    let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+    let startPosition = window.pageYOffset;
+    let startTime = null;
 
+    const ease = function (t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return (c / 2) * t * t + b;
+      t--;
+      return (-c / 2) * (t * (t - 2) - 1) + b;
+    };
+
+    const animation = function (currentTime) {
+      if (startTime === null) startTime = currentTime;
+      const timeElapsed = currentTime - startTime;
+      const run = ease(timeElapsed, startPosition, targetPosition, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
+    };
+    requestAnimationFrame(animation);
   };
 
   const scrollTo = function () {
-      const links = document.querySelectorAll('.js-scroll');
-      links.forEach(each => {
-          each.addEventListener('click', function () {
-              const currentTarget = this.getAttribute('href');
-              smoothScroll(currentTarget, 1000);
-          });
+    const links = document.querySelectorAll(".js-scroll");
+    links.forEach((each) => {
+      each.addEventListener("click", function () {
+        const currentTarget = this.getAttribute("href");
+        smoothScroll(currentTarget, 1000);
       });
+    });
   };
   scrollTo();
-}());
+})();
